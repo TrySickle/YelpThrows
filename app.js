64db514decb86b51a29d1d514fc3e2a3f5ca0830
Jason Lee <jason.khn.lee@gmail.com>
2018-02-04 03:33:32 -0500

    Styling

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var express     = require("express"),
 mongoose.connect("mongodb://localhost/yelp_throws");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-    
+app.use(express.static(__dirname + "/public"));
 seedDB();
 // ROUTES
 app.get("/", function(req, res) {
